7472d064c85d2a4f059c1209fa74be7ae148f1f1
SifatSakib007 <sifatsakib26@gmail.com>
2024-04-20 23:31:45 +0600

    Added solution to the problem no 10 on Hacker Rank.

---
+++ HackerRank/Problem 10/Given a time in -hour AMPM format, convert it to military (24-hour) time..cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        // Parse the input string into a DateTime object
+        DateTime time = DateTime.ParseExact(s, "h:mm:sstt", null);
+
+        // Convert the DateTime object to 24-hour format and return as string
+        return time.ToString("HH:mm:ss");
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
