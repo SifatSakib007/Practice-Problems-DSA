2d061054dd7c019842c9b56dbaddcbba3e5b894e
SifatSakib007 <sifatsakib26@gmail.com>
2024-04-27 22:18:05 +0600

    Added solution to the problem no 39 on hacker rank.

---
+++ HackerRank/Problem 39/Append and Delete.cs
@@ -0,0 +1,69 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'appendAndDelete' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. STRING t
+     *  3. INTEGER k
+     */
+
+    public static string appendAndDelete(string s, string t, int k)
+    {
+        int i = 0;
+        while (i < s.Length && i < t.Length && s[i] == t[i])
+        {
+            i++;
+        }
+        
+        int deleteOperations = s.Length - i;
+        int appendOperations = t.Length - i;
+        
+        if (deleteOperations + appendOperations > k)
+            return "No";
+        
+        if ((k - deleteOperations - appendOperations) % 2 == 0 || k >= s.Length + t.Length)
+            return "Yes";
+        
+        return "No";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string t = Console.ReadLine();
+
+        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.appendAndDelete(s, t, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
