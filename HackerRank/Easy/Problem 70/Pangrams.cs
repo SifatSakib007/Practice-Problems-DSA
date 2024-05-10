f916e8296f3c7a4c912db70ccf8b16228b126cf2
SifatSakib007 <sifatsakib26@gmail.com>
2024-05-10 11:27:47 +0600

    Added solution to the problem no 70 on hacker rank easy.

---
+++ HackerRank/Easy/Problem 70/Pangrams.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string pangrams(string s)
+{
+    // Convert the input string to lowercase and remove non-alphabetic characters
+    s = new string(s.ToLower().Where(char.IsLetter).Distinct().ToArray());
+
+    // Check if the length of the filtered string is equal to 26 (all letters of the alphabet)
+    return s.Length == 26 ? "pangram" : "not pangram";
+}
+
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
