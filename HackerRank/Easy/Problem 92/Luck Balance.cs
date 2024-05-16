062f7b73594936c635f39f46d4a8394da75e7d8f
SifatSakib007 <sifatsakib26@gmail.com>
2024-05-16 18:07:35 +0600

    Added solution no 92 hacker rank (Easy)

---
+++ HackerRank/Easy/Problem 92/Luck Balance.cs
@@ -0,0 +1,76 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'luckBalance' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. 2D_INTEGER_ARRAY contests
+     */
+
+    public static int luckBalance(int k, List<List<int>> contests)
+    {
+        int totalLuck = 0;
+
+        var importantContests = new List<int>();
+
+        foreach (var contest in contests)
+        {
+            if (contest[1] == 1)
+                importantContests.Add(contest[0]);
+            else
+                totalLuck += contest[0];
+        }
+
+        importantContests.Sort((a, b) => b.CompareTo(a));
+
+        totalLuck += importantContests.Take(k).Sum();
+        totalLuck -= importantContests.Skip(k).Sum();
+
+        return totalLuck;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> contests = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            contests.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(contestsTemp => Convert.ToInt32(contestsTemp)).ToList());
+        }
+
+        int result = Result.luckBalance(k, contests);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
